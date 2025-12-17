ecd039c75b125115a07f82dde61dc0259b90f3ba
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-17 11:48:01 +0100

    Add getActiveFeatures() function

--- app/database/queries.php
+++ app/database/queries.php
@@ -8,3 +8,11 @@ function getAllRooms(PDO $database): array
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function getActiveFeatures(PDO $database): array
+{
+    //TODO: LÄGG TILL CHECK OM DEN ÄR ACTIVE ELLER EJ///////////////////////////////
+    $statement = $database->prepare('SELECT * FROM features');
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
