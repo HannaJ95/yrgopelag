6c6b6c3be63dfaf42cfa97b7c6777c89dd9a74eb
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-17 11:52:34 +0100

    Add booking section with input fields and options

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/app/autoload.php';
 
 $rooms = getAllRooms($database);
+$features = getActiveFeatures($database);
 
 ?>
 
@@ -16,19 +17,63 @@ $rooms = getAllRooms($database);
 </head>
 <body>
 
+    <!--**** ROOM SECTION ****-->
     <section class="card_container">
         <?php foreach ($rooms as $room) : ?>
 
             <div class="card">
                 <h1 class="card_h1"><?= $room['name'] ?></h1>
                 <h2 class="card_h2"><?= $room['category'] ?></h2>
-                <div class="card_img_holder">
+                <div class="card_img-holder">
                     <img class="card_img">
                 </div>
                 <p><?= $room['price'] ?></p>
+                <div class="card_calendar"></div>
+                
+                
             </div>
-            <div class="card_calendar"></div>
-            <?php endforeach ?>
+        <?php endforeach ?>
+    </section>
+
+    <!--**** BOOKING SECTION ****-->
+    <section class="booking_container">
+        <form class="booking" action="app/posts/create-booking.php" method="post">
+
+            <!-- ask for guestname -->
+            <label for="name">Your name (guest_id)</label>
+            <input type="text" id="name" name="name"/>
+            
+            <!-- ask for transfer-code-->
+            <label for="transfercode">Transfer-code:</label>
+            <input type="text" id="transfercode" name="transfercode"/>
+            
+            <!-- print features-list -->
+            <label for="feature">Features</label>
+                <?php foreach ($features as $feature) : ?>
+                    <div>
+                        <input type="checkbox" id="feature-<?= $feature['id'] ?>" name="feature[]" value="<?= $feature['id'] ?>">
+                        <label for="feature-<?= $feature['id'] ?>"><?= $feature['name'] ?></label>
+                    </div>
+                <?php endforeach ?>
+
+            <!-- room dropdown -->
+            <label for="room_id">Pick room</label>
+            <select name="room_id">
+                <?php foreach ($rooms as $room) : ?>
+                    <option value="<?= $room['id'] ?>"><?= $room['category'] ?></option>
+                <?php endforeach ?>
+            </select>
+            
+            <!-- Arrival/Departure calendar -->
+            <label for="arrival">Arrival</label>
+            <input type="date" name="arrival" class="arrival" id="arrival" min="2026-01-01" max="2026-01-31">
+            
+            <label for="departure">Departure</label>
+            <input type="date" name="departure" class="departure" id="departure" min="2026-01-01" max="2026-01-31">
+
+
+            <button type="submit" class="btn">BOOK</button>
+        </form>
     </section>
     
 </body>
