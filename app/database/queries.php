dddff514565387c21af9a1e95550bde60f4a07c1
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-25 18:16:00 +0100

    Add function to get booked days

--- app/database/queries.php
+++ app/database/queries.php
@@ -16,3 +16,30 @@ function getActiveFeatures(PDO $database): array
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
+
+
+function getBookedDaysForRoom(PDO $database, int $room_id): array
+{
+    $stmt = $database->prepare("
+        SELECT arrival_date, departure_date 
+        FROM bookings 
+        WHERE room_id = ?
+    ");
+    
+    $stmt->execute([$room_id]);
+    $bookings = $stmt->fetchAll();
+    
+    $booked_days = [];
+    
+    foreach ($bookings as $booking) {
+        $start = new DateTime($booking['arrival_date']);
+        $end = new DateTime($booking['departure_date']);
+        
+        while ($start < $end) {
+            $booked_days[] = (int) $start->format('j');
+            $start->modify('+1 day');
+        }
+    }
+    
+    return $booked_days;
+}
