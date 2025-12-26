7e7ef4b35cee32a6b9a3b8785ebf9a312652bad0
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-26 17:42:19 +0100

    Add days for calendar

--- app/config.php
+++ app/config.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 return [
     'database_path' => sprintf('sqlite:%s/database/database.db', __DIR__),
     'centralbank_api' => ('https://www.yrgopelag.se/centralbank/'),
-    'user' => 'Hanna'
+    'user' => 'Hanna',
+    'calendar_days' => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 ];
