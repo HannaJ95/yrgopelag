acf81adabc48ece7e9180fa95d8c1eb37fe5780b
Hanna Johansson <hannajohansson16@hotmail.com>
2026-01-01 19:41:44 +0100

    Add verify login for admin

---
+++ app/admin-users/login.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../autoload.php';
+
+// Check if email and password exists in database
+if (isset($_POST['email'], $_POST['password'])) {
+    $email = trim($_POST['email']);
+
+    $statement = $database->prepare('SELECT * FROM admin_users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    // if admin-user don't exist, svare error in $_SESSION
+    if (!$user) {
+        $_SESSION['admin']['error'] = "User does not exist please try again";
+        redirect('/admin/index.php');
+
+    }
+
+    //verify password and save user in $_SESSION
+    if (password_verify($_POST['password'], $user['password'])) {
+
+        unset($user['password']);
+
+        $_SESSION['user'] = $user;
+
+        unset($_SESSION['admin']['error']);
+    }
+}
+
+redirect('/admin/index.php');
