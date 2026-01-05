7f88f98a3dce8ec838c78279770d5b42933d999f
Hanna Johansson <hannajohansson16@hotmail.com>
2026-01-05 11:17:59 +0100

    Updated config file with paths for project files

--- app/config.php
+++ app/config.php
@@ -8,5 +8,28 @@ return [
     'user' => 'Hanna',
     'calendar_days' => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     'base_url' => '/yrgopelag',
-    'hotel_name' => "LOST ISLAND HOTEL"
+    'hotel_name' => "LOST ISLAND HOTEL",
+
+    'paths' => [
+        'index' => '/index.php',
+        'receipt' => '/receipt.php',
+        'admin' => [
+            'index' => '/admin/index.php',
+            'login' => '/admin/login.php'
+        ],
+        'posts' => [
+            'create_booking' => '/app/posts/create-booking.php',
+            'admin' => [
+                'update_price' => '/app/posts/admin/update-price.php',
+                'toggle_active' => '/app/posts/admin/toggle-active.php',
+                'insert_features' => '/app/posts/admin/insert-features.php',
+                'insert_package' => '/app/posts/admin/insert-package.php',
+                'login' => '/app/admin-users/login.php'
+            ]
+        ]
+    ],
+
+    'assets' => [
+        'css' => '/assets/styles/app.css'
+    ]
 ];
