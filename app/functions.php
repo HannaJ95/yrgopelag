10df89f2e1287f9df6a95c12bebe1dd799ecc8ae
Hanna Johansson <hannajohansson16@hotmail.com>
2026-01-08 18:44:32 +0100

    Add getIslandStars() function

--- app/functions.php
+++ app/functions.php
@@ -7,3 +7,23 @@ function redirect(string $path)
     header("Location: $path");
     exit;
 }
+
+
+function getIslandStars(int $island_id = 213): int
+{
+    global $client;
+
+    try {
+        $response = $client->request('GET', 'islands');
+        $islands = json_decode($response->getBody()->getContents(), true);
+
+        $key = array_search($island_id, array_column($islands, 'id'));
+        if ($key !== false && isset($islands[$key]['stars'])) {
+            return (int)$islands[$key]['stars'];
+        }
+    } catch (\Throwable $e) {
+        
+    }
+
+    return 0;
+}
