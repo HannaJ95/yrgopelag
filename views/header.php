ef19d24e942d69e308b3283322bdddae309d3d89
Hanna Johansson <hannajohansson16@hotmail.com>
2026-01-08 19:22:45 +0100

    Add get stars from centralbanken, updated paths

--- views/header.php
+++ views/header.php
@@ -1,24 +1,16 @@
 <?php
 
-
-// GET NUMBER OF HOTEL STARS FROM CENTRALBANKEN
-$response = $client->request('GET', 'islands');
-$islands = json_decode($response->getBody()->getContents(), true);
-
-$key = array_search(213, array_column($islands, 'id'));
-if ($key !== false) {
-    $stars = $islands[$key]['stars'];
-}
+$stars = (int)getIslandStars() ?? 0;
 
 ?>
 
 
 <header class="main_header">
-    <img class="header_logo" src="/assets/images/LOST_ISLAND_HOTEL1.png" alt="">
+    <img class="header_logo" src="<?= $config['assets']['images']['header_logo'] ?>" alt="">
     <div class="header_stars">
 
         <?php for ($i=0; $i < $stars; $i++) : ?> 
-            <img class="header_star" src="/assets/images/star1.png" alt="star">
+            <img class="header_star" src="<?= $config['assets']['images']['star'] ?>" alt="star">
         <?php endfor; ?>
     
     </div>
